a45cfaa290d77a7875d82014fdd55586f90cdb7c
avi <gajai.avinash@gmail.com>
2024-07-15 12:49:23 -0400

    creates a 16x16 square div using DOM

---
+++ code.js
@@ -0,0 +1,12 @@
+const NUMOFDIVS = 16*16;
+const cdiv = document.querySelector(".container"); /* .container selects elements by class name
+                                                       and #container selects elements by ID */
+
+
+ for (let i = 0; i <=NUMOFDIVS; i++) {
+    const div = document.createElement("div");
+    cdiv.appendChild(div);
+
+    
+}
+
