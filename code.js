3376bcc3711790e5b88a0bda41c1c8047c869eb0
avi <gajai.avinash@gmail.com>
2024-07-15 17:34:08 -0400

    created a button that resizes grid following user prompt

--- code.js
+++ code.js
@@ -1,25 +1,57 @@
-const NUMOFSQUARES = 16*16;
 const container = document.querySelector(".container"); /* .container selects elements by class name
                                                        and #container selects elements by ID */
 
 
- for (let i = 0; i <=NUMOFSQUARES; i++) {
-    const square = document.createElement("div"); 
-    square.classList.add('grid-square');
-    container.appendChild(square);
 
-    
+function createGrid(size) {
+    container.innerHTML = '';
+    const NUMOFSQUARES = size * size;
+
+    for (let i = 0; i <= NUMOFSQUARES; i++) {
+        const square = document.createElement("div");
+        square.classList.add('grid-square');
+        container.appendChild(square);
+
+
+    }
 }
-const squares = document.querySelectorAll('.grid-square');
-squares.forEach(square => {
-    square.addEventListener('mouseenter', function() {
-        this.classList.add('colored');
-    });
-})
 
-squares.forEach(square => {
-    square.addEventListener('mouseout', function() {
-        this.classList.toggle('differentcolored');
-    });
-})
+function etchSketch() {
+    const squares = document.querySelectorAll('.grid-square');
+    squares.forEach(square => {
+        square.addEventListener('mouseenter', function () {
+            this.classList.add('colored');
+        });
+    })
 
+    squares.forEach(square => {
+        square.addEventListener('mouseout', function () {
+            this.classList.toggle('differentcolored');
+        });
+    })
+}
+
+createGrid(16);
+etchSketch();
+
+
+
+const button = document.getElementById('btn');
+button.addEventListener('click', resizeGrid);
+
+
+function resizeGrid() {
+    let newSize = prompt("Enter a new grid size");
+    newSize = parseInt(newSize);
+
+    if (isNaN(newSize) || newSize <= 0) {
+        alert("Enter a valid number");
+        return;
+    }
+
+    container.innerHTML = '';
+    createGrid(newSize);
+    etchSketch();
+
+
+} 
