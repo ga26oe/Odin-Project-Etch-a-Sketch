aff75801878f54b9515fec787b5db0d260a1cfdd
avi <gajai.avinash@gmail.com>
2024-07-15 16:25:27 -0400

    changes color over over to aqua

--- code.js
+++ code.js
@@ -1,12 +1,24 @@
-const NUMOFDIVS = 16*16;
-const cdiv = document.querySelector(".container"); /* .container selects elements by class name
+const NUMOFSQUARES = 16*16;
+const container = document.querySelector(".container"); /* .container selects elements by class name
                                                        and #container selects elements by ID */
 
 
- for (let i = 0; i <=NUMOFDIVS; i++) {
-    const div = document.createElement("div");
-    cdiv.appendChild(div);
+ for (let i = 0; i <=NUMOFSQUARES; i++) {
+    const square = document.createElement("div"); 
+    square.classList.add('grid-square');
+    container.appendChild(square);
 
     
 }
+/* 
+document.querySelector("div").addEventListener( 
+    'mouseover', function() {
+        dcument.querySelector("div").classList.toggle("colored");
+    } ) */
 
+const squares = document.querySelectorAll('.grid-square');
+squares.forEach(square => {
+    square.addEventListener('mouseover', function(event) {
+        this.classList.toggle('colored');
+    });
+})
