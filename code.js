ee0dc994dae8053d7e3a1a3d85ab53b4e55af626
avi <gajai.avinash@gmail.com>
2024-07-15 18:03:10 -0400

    most of the code works but problem with sizing the grid

--- code.js
+++ code.js
@@ -1,19 +1,23 @@
 const container = document.querySelector(".container"); /* .container selects elements by class name
                                                        and #container selects elements by ID */
-
-
+const containerSize = 480;
 
 function createGrid(size) {
     container.innerHTML = '';
+    const squareSize = containerSize / size;
+
     const NUMOFSQUARES = size * size;
 
-    for (let i = 0; i <= NUMOFSQUARES; i++) {
+    for (let i = 0; i < NUMOFSQUARES; i++) { // Use < instead of <=
         const square = document.createElement("div");
         square.classList.add('grid-square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         container.appendChild(square);
-
-
     }
+
+    // Add event listeners to new squares
+    etchSketch();
 }
 
 function etchSketch() {
@@ -22,23 +26,14 @@ function etchSketch() {
         square.addEventListener('mouseenter', function () {
             this.classList.add('colored');
         });
-    })
+    });
 
     squares.forEach(square => {
         square.addEventListener('mouseout', function () {
             this.classList.toggle('differentcolored');
         });
-    })
+    });
 }
-
-createGrid(16);
-etchSketch();
-
-
-
-const button = document.getElementById('btn');
-button.addEventListener('click', resizeGrid);
-
 
 function resizeGrid() {
     let newSize = prompt("Enter a new grid size");
@@ -49,9 +44,12 @@ function resizeGrid() {
         return;
     }
 
-    container.innerHTML = '';
     createGrid(newSize);
-    etchSketch();
+}
 
+// Initial grid creation
+createGrid(16);
 
-} 
+const button = document.getElementById('btn');
+button.addEventListener('click', resizeGrid);
+
